b93886450ff1506c4ba161a2d6b317f5fe6d329a
onyyyyy <hongki365@gmail.com>
2023-11-27 23:55:16 +0900

    구조완성 포스터 사진 넣기 전, 포스터 연결해야함

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,46 +2,7 @@ import { useEffect, useState } from "react";
 import { Loading } from "../../components/Loading";
 import { nowPlaying, topRated } from "../../api";
 import { Banner } from "./Banner";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { routes } from "../../routes";
-
-const Wrap = styled.div`
-  padding: 100px 5%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-const Title = styled.h3`
-  font-size: 80px;
-  font-weight: 900;
-`;
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin: 80px 0 50px 0;
-`;
-const ConWarp = styled.div`
-  width: 30%;
-  h4 {
-    margin-top: 20px;
-    font-size: 30px;
-    font-weight: 600;
-  }
-`;
-const Con = styled.div`
-  width: 100%;
-  height: 500px;
-  background-color: lightcoral;
-`;
-const ViewBtn = styled.button`
-  all: unset;
-  padding: 15px 20px;
-  background-color: #808080;
-  border-radius: 30px;
-`;
+import { TopRated } from "./TopRated";
 
 export const Home = () => {
   const [nowPlayingData, setNowPlayingData] = useState();
@@ -77,26 +38,7 @@ export const Home = () => {
           {nowPlayingData && (
             <>
               <Banner bannerData={nowPlayingData[0]} />
-              <Wrap>
-                <Title>Top Rated</Title>
-                <Container>
-                  <ConWarp>
-                    <Con></Con>
-                    <h4>{topRatedData[0].title}</h4>
-                  </ConWarp>
-                  <ConWarp>
-                    <Con></Con>
-                    <h4>{topRatedData[1].title}</h4>
-                  </ConWarp>
-                  <ConWarp>
-                    <Con></Con>
-                    <h4>{topRatedData[2].title}</h4>
-                  </ConWarp>
-                </Container>
-                <ViewBtn>
-                  <Link to={routes.TopRatedPage}>View More +</Link>
-                </ViewBtn>
-              </Wrap>
+              <TopRated topData={topRatedData} />
             </>
           )}
         </>
--- src/pages/Home/TopRated.js
+++ src/pages/Home/TopRated.js
@@ -1,6 +1,80 @@
 import { Link } from "react-router-dom";
 import { routes } from "../../routes";
+import styled from "styled-components";
+import { IMG_URL } from "../../constants";
 
-export const TopRated = () => {
-  return <div></div>;
+const Wrap = styled.div`
+  padding: 100px 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h3`
+  font-size: 80px;
+  font-weight: 900;
+`;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 80px 0 50px 0;
+`;
+const ConWarp = styled.div`
+  width: 30%;
+  h4 {
+    margin-top: 20px;
+    font-size: 30px;
+    font-weight: 600;
+  }
+`;
+const Con = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: lightcoral;
+  /* &:first-child {
+    background: url(${IMG_URL}/w500/${(props) => props.$bgUrl1}) no-repeat
+      center / cover;
+  } */
+
+  &:nth-child(2) {
+    background: url(${IMG_URL}/w500/${(props) => props.$bgUrl2}) no-repeat
+      center / cover;
+  }
+
+  /* &:last-child {
+    background: url(${IMG_URL}/w500/${(props) => props.$bgUrl3}) no-repeat
+      center / cover;
+  } */
+`;
+const ViewBtn = styled.button`
+  all: unset;
+  padding: 15px 20px;
+  background-color: #808080;
+  border-radius: 30px;
+`;
+
+export const TopRated = ({ topData }) => {
+  return (
+    <Wrap>
+      <Title>Top Rated</Title>
+      <Container>
+        <ConWarp>
+          <Con></Con>
+          <h4>{topData[0].title}</h4>
+        </ConWarp>
+        <ConWarp>
+          <Con></Con>
+          <h4>{topData[1].title}</h4>
+        </ConWarp>
+        <ConWarp>
+          <Con></Con>
+          <h4>{topData[2].title}</h4>
+        </ConWarp>
+      </Container>
+      <ViewBtn>
+        <Link to={routes.TopRatedPage}>View More +</Link>
+      </ViewBtn>
+    </Wrap>
+  );
 };
